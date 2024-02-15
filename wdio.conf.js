408c7f55fb2bb5586f1da0f4256857ddfefdecf8
QuinnBurt <icevapor096@gmail.com>
2024-02-15 14:54:34 -0500

    Add 8 unique tests that cover various levels of functionality with the Urban.Routes app

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
 ],
     logLevel: 'error',
     bail: 0,
-    baseUrl: 'https://cnt-f0404c04-be65-4653-b623-3238dc2b1640.containerhub.tripleten-services.com',
+    baseUrl: 'https://cnt-6156ed7b-f546-46a9-93cd-43760020cd94.containerhub.tripleten-services.com',
     waitforTimeout: 6000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
